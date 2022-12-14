5e6026eef46fec704b47272b30d14138be62b864
qucumbah <39967396+qucumbah@users.noreply.github.com>
2022-12-14 20:53:23 +0300

    clean up unsused files & add some meta tags

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,7 @@ export default function HomeHead() {
   return (
     <>
       <DefaultHead />
-      <title>Workoutly</title>
+      <title>Workoutly - a gym progress tracker</title>
     </>
   );
 }
--- src/components/DefaultHead.tsx
+++ src/components/DefaultHead.tsx
@@ -2,6 +2,10 @@ export default function DefaultHead() {
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Gym tracker app built using Next.js 13 with app directory" />
+      <meta name="keywords" content="gym progress app Next.js 13" />
+      <meta name="theme-color" content="#3B82F6" />
+      <link rel="icon" href="/icon.svg" />
     </>
   );
 }
